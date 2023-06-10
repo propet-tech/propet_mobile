b0bfe125ae8d9e5dbf175779b6f230ff4b5384d6
deividrx <galdinodeivid@gmail.com>
2023-06-10 19:59:37 -0300

    refactor: user multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:propet_mobile/core/app_config_provider.dart';
 import 'package:propet_mobile/core/app_state.dart';
 import 'package:propet_mobile/core/dependencies.dart';
+import 'package:propet_mobile/core/providers/cart_provider.dart';
 import 'package:propet_mobile/core/routes.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
+  Intl.defaultLocale = 'pt_BR';
   WidgetsFlutterBinding.ensureInitialized();
 
   await Future.wait([
     configureDependencies(),
-    getIt<AppState>().autoLogin()
+    getIt<AppState>().autoLogin(),
+    initializeDateFormatting(),
   ]);
 
   runApp(const ProPetApp());
@@ -21,11 +26,15 @@ class ProPetApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => AppConfig(),
-      builder: (context, _) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: getIt<AppState>()),
+        ChangeNotifierProvider(create: (ctx) => CartProvider()),
+        ChangeNotifierProvider(create: (ctx) => AppConfig())
+      ],
+      builder: (ctx, _) {
         return MaterialApp.router(
-          themeMode: context.watch<AppConfig>().mode,
+          themeMode: ctx.watch<AppConfig>().mode,
           debugShowCheckedModeBanner: false,
           theme: ThemeData(useMaterial3: true, brightness: Brightness.light),
           darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
@@ -33,14 +42,6 @@ class ProPetApp extends StatelessWidget {
           routerDelegate: routes.routerDelegate,
           routeInformationParser: routes.routeInformationParser,
           routeInformationProvider: routes.routeInformationProvider,
-          builder: (context, child) {
-            return MultiProvider(
-              providers: [
-                ChangeNotifierProvider.value(value: getIt<AppState>())
-              ],
-              child: child,
-            );
-          },
         );
       },
     );
