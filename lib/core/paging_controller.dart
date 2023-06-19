11e572e1d8c16d6bab82e98883b9c4a5adfa56f6
deividrx <galdinodeivid@gmail.com>
2023-06-19 09:41:26 -0300

    feat: change core components

--- lib/core/paging_controller.dart
+++ lib/core/paging_controller.dart
@@ -1,7 +1,7 @@
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
-class CustomPagingController<PageKeyType, ItemType>
-    extends PagingController<PageKeyType, ItemType> {
+class CustomPagingController<T>
+    extends PagingController<int, T> {
   CustomPagingController({required super.firstPageKey});
 
   void removeAt(int index) {
