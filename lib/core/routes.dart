3559139d504af2238ae920adf6df22dfda6483b6
deividrx <galdinodeivid@gmail.com>
2023-06-19 09:43:53 -0300

    feat: use StatefulShellRoute to keep state across pages

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:propet_mobile/core/app_state.dart';
 import 'package:propet_mobile/core/dependencies.dart';
 import 'package:propet_mobile/core/services/auth_service.dart';
 import 'package:propet_mobile/models/pet/pet.dart';
+import 'package:propet_mobile/models/petshop_service.dart';
 import 'package:propet_mobile/pages/config/config_page.dart';
 import 'package:propet_mobile/pages/entry/entry_point.dart';
 import 'package:propet_mobile/pages/home/home_page.dart';
 import 'package:propet_mobile/pages/login/login_page.dart';
 import 'package:propet_mobile/pages/order/cart_list.dart';
-import 'package:propet_mobile/pages/order/order_list.dart';
 import 'package:propet_mobile/pages/order/track_pet.dart';
-import 'package:propet_mobile/pages/pedido_page.dart';
 import 'package:propet_mobile/pages/pet/pet_detail.dart';
 import 'package:propet_mobile/pages/pet/pet_list_page.dart';
+import 'package:propet_mobile/pages/service/pedido_page.dart';
+import 'package:propet_mobile/pages/service/service_page.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 final _shellNavigatorKey = GlobalKey<NavigatorState>();
@@ -21,7 +21,7 @@ final _shellNavigatorKey = GlobalKey<NavigatorState>();
 final routes = GoRouter(
   initialLocation: "/login",
   navigatorKey: _rootNavigatorKey,
-  refreshListenable: getIt<AppState>(),
+  refreshListenable: getIt<AuthService>(),
   redirect: (context, state) {
     bool isAuthenticated = getIt<AuthService>().isAuthenticated();
 
@@ -45,62 +45,68 @@ final routes = GoRouter(
       parentNavigatorKey: _rootNavigatorKey,
       builder: (context, state) => const ConfigPage(),
     ),
-    ShellRoute(
-      navigatorKey: _shellNavigatorKey,
-      builder: (context, state, child) {
-        return ScaffoldNavBar(child: child);
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return ScaffoldNavBar(navigationShell: navigationShell);
       },
-      routes: [
-        GoRoute(
-          path: "/orders",
-          builder: (context, state) {
-            return Pedidos();
-          },
-          routes: [
-            GoRoute(
-              path: "track/:id",
-              parentNavigatorKey: _rootNavigatorKey,
-              builder: (context, state) {
-                return PetTrack(id: int.parse(state.pathParameters['id']!));
-              },
-            ),
-            GoRoute(
-              path: "cart",
-              parentNavigatorKey: _rootNavigatorKey,
-              builder: (context, state) {
-                return CartPage();
-              },
-            ),
-            GoRoute(
-              path: "new",
-              parentNavigatorKey: _rootNavigatorKey,
-              builder: (context, state) {
-                return NewOrder();
-              },
-            )
-          ],
-        ),
-        GoRoute(
-          path: "/home",
-          builder: (context, state) {
-            return  HomePage();
-          },
-        ),
-        GoRoute(
-            path: "/pets",
+      branches: [
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: "/home",
             builder: (context, state) {
+              return HomePage();
+            },
+          ),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(
+              path: "/pets",
+              builder: (context, state) {
                 return PetListPage();
+              },
+              routes: [
+                GoRoute(
+                  path: "edit",
+                  parentNavigatorKey: _rootNavigatorKey,
+                  builder: (context, state) {
+                    final pet = state.extra as Pet?;
+                    return PetDetailPage(pet: pet);
+                  },
+                ),
+              ]),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: "/orders",
+            builder: (context, state) {
+              return Pedidos();
             },
             routes: [
               GoRoute(
-                path: "edit",
+                path: "track/:id",
                 parentNavigatorKey: _rootNavigatorKey,
                 builder: (context, state) {
-                  final pet = state.extra as Pet?;
-                  return PetDetailPage(pet: pet);
+                  return PetTrack(id: int.parse(state.pathParameters['id']!));
                 },
               ),
-            ]),
+              GoRoute(
+                path: "cart",
+                parentNavigatorKey: _rootNavigatorKey,
+                builder: (context, state) {
+                  return CartPage();
+                },
+              ),
+              GoRoute(
+                path: "new",
+                parentNavigatorKey: _rootNavigatorKey,
+                builder: (context, state) {
+                  final service = state.extra as PetShopServiceDto;
+                  return ServicePage(service: service,);
+                },
+              )
+            ],
+          ),
+        ]),
       ],
     )
   ],
